cfa1a7e6b8ca24fcf915c9d5b8d8e274a43c0dcc
Diksha <erdiksha58@gmail.com>
2021-02-16 03:40:45 +0530

    update

---
+++ Procfile
@@ -0,0 +1,1 @@
+web : gunicorn zeetdjango.wsgi --log-file -
--- zeetdjango/settings.py
+++ zeetdjango/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '24zu$@5_%)01gn10o!pu@b&djtk&dxo8clt_$wgbd*b+_4ole5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','try-webb.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
